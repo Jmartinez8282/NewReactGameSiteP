00297593a4352a783d5e79f3e8a88f48ff70b957
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2023-08-04 21:31:25 -0700

    Added ColorSchem fontsize and padding and border radius to the badge

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react'
+import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react'
 import PlatformIconsList from './PlatformIconsList'
+import CriticScore from './CriticScore'
 
 interface GameProps {
     game: Game
@@ -13,7 +14,11 @@ const GameCard = ({game} : GameProps) => {
         <Image src={game.background_image}/>
         <CardBody>
             <Heading fontSize='2xl'>{game.name}</Heading>
+            <HStack justifyContent='space-between'>
            <PlatformIconsList platform={game.parent_platforms.map(p => p.platform)}/>
+           <CriticScore score={game.metacritic}/>
+
+            </HStack>
 
         </CardBody>
     </Card>
