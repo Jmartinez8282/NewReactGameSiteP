9e997ab8780f3fa0fa8148705f34ccc6abf90f4a
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2023-08-05 21:07:09 -0700

    Added Loading Skeletons from Chakra Ui

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,23 +1,27 @@
-
-import { SimpleGrid, Text } from '@chakra-ui/react';
-import useGames from '../hooks/useGames';
-import GameCard from './GameCard';
-
-
+import { SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
+import GameCardSkeloton from "./GameCardSkeloton";
 
 const GameGrid = () => {
-
-  const {games, error}= useGames();
+  const { games, error, isLoading } = useGames();
+  const skelotons = [1,2,3,4,5,6,7,8,9,10];
 
   return (
     <>
-    {error && <Text>{error}</Text>}
-     <SimpleGrid columns={{sm:1, md: 2, lg:3, xl:5 }} padding='20px' spacing={10}>
-        {games.map((game) => (<GameCard key={game.id} game={game}/>))}
-     </SimpleGrid>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="20px"
+        spacing={10}
+      >
+        {isLoading && skelotons.map(skeleton => <GameCardSkeloton key={skeleton}/>)}
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </SimpleGrid>
     </>
-    
-  )
-}
+  );
+};
 
-export default GameGrid
+export default GameGrid;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-
 export interface Platform {
   id: number;
   name: string;
@@ -13,7 +12,7 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: {platform: Platform}[];
+  parent_platforms: { platform: Platform }[];
   metacritic: number;
 }
 
@@ -24,20 +23,27 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+
+    setIsLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((response) => setGames(response.data.results))
+      .then((response) => {
+        setGames(response.data.results);
+        setIsLoading(false);
+      })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message);
+        setIsLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 export default useGames;
